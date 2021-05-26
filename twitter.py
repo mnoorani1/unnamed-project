f174c23f87a03dec1b734ebe393ddacce5a4e811
Moiz Noorani <m.noorani@fiplan.de>
2021-05-26 16:21:46 +0200

    added twitter scraping

---
+++ twitter.py
@@ -0,0 +1,12 @@
+import tweepy
+from tweepy import OAuthHandler
+from datetime import datetime, date
+
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
